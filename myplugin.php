11acc119fde6e0612a367335b1c9cf4ef4b76b76
benn payne <bennpayne@gmail.com>
2019-03-21 15:28:54 -0500

    initial commit

---
+++ myplugin.php
@@ -0,0 +1,22 @@
+<?php
+/*
+Plugin Name:  MyPlugin
+Description:  Example plugin from the video tutorial series, "WordPress: Plugin Development", available at Lynda.com.
+Plugin URI:   https://hereismy.com
+Author:       Benjamen Payne
+Version:      1.0.1
+Text Domain:  myplugin
+Domain Path:  /languages
+License:      GPL v2 or later
+License URI:  https://www.gnu.org/licenses/gpl-2.0.txt
+*/
+
+
+// exit if file is called directly
+if ( ! defined( 'ABSPATH' ) ) {
+
+	exit;
+
+}
+
+
